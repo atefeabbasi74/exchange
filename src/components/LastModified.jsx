7032795ec05f3c3d7b0d14f81758cfb9e5e009b8
AtefeAbbasi74 <atefe.abbasi74@yahoo.com>
2020-12-17 22:17:19 +0330

    edit style LastModified components and fetching data

--- src/components/LastModified.jsx
+++ src/components/LastModified.jsx
@@ -1,9 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 const LastModified = () => {
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        Axios.get('https://currency.jafari.li/json')
+        .then((res) => setData(res.data.LastModified))
+    }, [])
+
     return ( 
-        <div>
-            آخرین بروزرسانی
+        <div className="LastModified">
+            آخرین بروزرسانی : {' '}
+            {data}
         </div>
      );
 }
